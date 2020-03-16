1ce51790138e73e10056090c332c311b1847c3d9
Lamine Sané <aminesane18@gmail.com>
2020-03-16 23:41:08 +0000

    backend mounted !

---
+++ resources/views/backend/pages/home.blade.php
@@ -0,0 +1,15 @@
+@extends('backend.layouts.global')
+
+@section('title', 'Admin')
+    
+@section('css')
+    
+@endsection
+
+@section('content')
+    
+@endsection
+
+@section('javascript')
+    
+@endsection
