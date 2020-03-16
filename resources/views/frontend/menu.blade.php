60f0db6c432ea8bc7ca73d209c39218e0bd7b895
Lamine Sané <aminesane18@gmail.com>
2020-03-16 23:00:56 +0000

    First commit !

---
+++ resources/views/frontend/menu.blade.php
@@ -0,0 +1,31 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">citrone</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="oi oi-menu"></span> Menu
+        </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item {{ (request()->is('/')) ? 'active' : '' }}"><a href="/" class="nav-link">Accueil</a></li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown04">
+                <a class="dropdown-item" href="shop.html">Shop</a>
+                <a class="dropdown-item" href="wishlist.html">Wishlist</a>
+                <a class="dropdown-item" href="product-single.html">Single Product</a>
+                <a class="dropdown-item" href="cart.html">Cart</a>
+                <a class="dropdown-item" href="checkout.html">Checkout</a>
+                </div>
+            </li>
+            <li class="nav-item {{ (request()->is('/about')) ? 'active' : '' }}"><a href="/about" class="nav-link">A propos</a></li>
+            <li class="nav-item {{ (request()->is('/blog')) ? 'active' : '' }}"><a href="/blog" class="nav-link">Blog</a></li>
+            <li class="nav-item {{ (request()->is('/contact')) ? 'active' : '' }}"><a href="/contact" class="nav-link">Contact</a></li>
+            <li class="nav-item cta cta-colored"><a href="cart.html" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
+
+        </ul>
+        </div>
+    </div>
+</nav>
+<!-- END nav -->
