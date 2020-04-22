a5a98815772f71ff9f2f9bacbb9e95365cedb507
Lamine Sané <aminesane18@gmail.com>
2020-04-22 14:44:52 +0000

    aaa

--- resources/views/backend/sidebar.blade.php
+++ resources/views/backend/sidebar.blade.php
@@ -3,29 +3,11 @@
     <div class="pcoded-inner-navbar main-menu">
         <div class="pcoded-navigatio-lavel">Navigation</div>
         <ul class="pcoded-item pcoded-left-item">
-            <li class="pcoded-hasmenu active pcoded-trigger">
-                <a href="javascript:void(0)">
-                    <span class="pcoded-micon"><i class="feather icon-home"></i></span>
+            <li class="">
+                <a href="/admin">
+                    <span class="pcoded-micon"><i class="feather icon-menu"></i></span>
                     <span class="pcoded-mtext">Dashboard</span>
                 </a>
-                <ul class="pcoded-submenu">
-                    <li class="">
-                        <a href="index-1.htm">
-                            <span class="pcoded-mtext">Default</span>
-                        </a>
-                    </li>
-                    <li class="active">
-                        <a href="dashboard-crm.htm">
-                            <span class="pcoded-mtext">CRM</span>
-                        </a>
-                    </li>
-                    <li class="">
-                        <a href="dashboard-analytics.htm">
-                            <span class="pcoded-mtext">Analytics</span>
-                            <span class="pcoded-badge label label-info ">NEW</span>
-                        </a>
-                    </li>
-                </ul>
             </li>
             <li class="pcoded-hasmenu">
                 <a href="javascript:void(0)">
@@ -35,42 +17,42 @@
                 </a>
                 <ul class="pcoded-submenu">
                     <li class=" ">
-                        <a href="menu-bottom.htm">
+                        <a href="#">
                             <span class="pcoded-mtext">Stock</span>
                         </a>
                     </li>
                     <li class=" ">
-                        <a href="box-layout.htm" target="_blank">
+                        <a href="#" target="_blank">
                             <span class="pcoded-mtext">Vendu</span>
                         </a>
                     </li>
                     <li class=" ">
-                        <a href="menu-rtl.htm" target="_blank">
+                        <a href="#" target="_blank">
                             <span class="pcoded-mtext">Invendu</span>
                         </a>
                     </li>
                 </ul>
             </li>
             <li class="">
-                <a href="navbar-light.htm">
+                <a href="#">
                     <span class="pcoded-micon"><i class="feather icon-menu"></i></span>
                     <span class="pcoded-mtext">Commandes</span>
                 </a>
             </li>
             <li class="pcoded-hasmenu">
-                <a href="javascript:void(0)">
+                <a href="#">
                     <span class="pcoded-micon"><i class="feather icon-layers"></i></span>
                     <span class="pcoded-mtext">Charges</span>
                     <span class="pcoded-badge label label-danger">100+</span>
                 </a>
                 <ul class="pcoded-submenu">
                     <li class=" ">
-                        <a href="widget-statistic.htm">
+                        <a href="#">
                             <span class="pcoded-mtext">Internes</span>
                         </a>
                     </li>
                     <li class=" ">
-                        <a href="widget-data.htm">
+                        <a href="#">
                             <span class="pcoded-mtext">Externes</span>
                         </a>
                     </li>
@@ -80,19 +62,19 @@
         <div class="pcoded-navigatio-lavel">UI Element</div>
         <ul class="pcoded-item pcoded-left-item">
             <li class=" ">
-                <a href="animation.htm">
+                <a href="#">
                     <span class="pcoded-micon"><i class="feather icon-aperture"></i><b>A</b></span>
                     <span class="pcoded-mtext">Users</span>
                 </a>
             </li>
             <li class=" ">
-                <a href="animation.htm">
+                <a href="#">
                     <span class="pcoded-micon"><i class="feather icon-aperture"></i><b>A</b></span>
                     <span class="pcoded-mtext">Clients</span>
                 </a>
             </li>
             <li class=" ">
-                <a href="sticky.htm">
+                <a href="#">
                     <span class="pcoded-micon"><i class="feather icon-cpu rotate-refresh"></i></span>
                     <span class="pcoded-mtext">Documentation</span>
                     {{-- <span class="pcoded-badge label label-danger">HOT</span> --}}
